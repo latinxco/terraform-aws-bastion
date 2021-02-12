e5fc09ae016cee46686a4d609f4e11778145f849
Arijit Dey <arijit.dey18@icloud.com>
2021-02-11 19:36:12 -0800

    terraform upgrade

--- data.tf
+++ data.tf
@@ -1,8 +1,7 @@
 data "aws_ami" "amazon-linux-2" {
   most_recent = true
   owners      = ["amazon"]
-
-  name_regex = "^amzn2-ami-hvm.*-ebs"
+  name_regex  = "^amzn2-ami-hvm.*-ebs"
 
   filter {
     name   = "architecture"
